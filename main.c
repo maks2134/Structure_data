903b0204a5ad1fce8e85cd89beb8a7a223997497
Maxim Kozlov <maks210306@yandex.ru>
2024-02-19 12:49:38 +0300

    Создания основы

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+struct temporarry_struct{
+    int id;
+    char name;
+    int date;
+    char user;
+    int hoock;
+};
+
+void addData(){
+
+}
+
+void removeData(){
+
+}
+
+void checkData(){
+
+}
+
+void printData(){
+
+}
+
+
+int main(int argc, char *argv[]) {
+
+    return 0;
+}
